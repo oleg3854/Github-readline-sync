5d6b2c7537da7ff01385a7e1fa58fbdf3a91fbea
Oleg <zmey3854@gmail.com>
2023-11-22 16:09:52 +0300

    a

--- task3.js
+++ task3.js
@@ -7,7 +7,7 @@ const userDiscount = readlineSync.question('Введите скидку: ');
 //Объявление функции
 const calculateDiscount = (price, discountPercentage) => {
   const discount = (price * discountPercentage) / 100;
-  return (`${price} + ${discount}`);
+  return price - discount;
 }
 
 //Вывод результата
